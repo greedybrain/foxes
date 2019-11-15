eacfe225966fe3d4b04a33bd7fa1d7e2554b50d5
allisonkadel <akadel01@gmail.com>
2019-11-15 09:32:24 -0500

    Add ORM code

--- ORM-Review/config/environment.rb
+++ ORM-Review/config/environment.rb
@@ -3,4 +3,6 @@ require 'pry'
 
 DB = { :conn => SQLite3::Database.new('db/cats.db') }
 
+DB[:conn].results_as_hash = true
+
 require_relative '../lib/cat'
--- ORM-Review/lib/cat.rb
+++ ORM-Review/lib/cat.rb
@@ -1,15 +1,28 @@
 # class method that creates a cats table if it doesn't exist
 # class method that finds a cat by its id and returns an instance of a cat with that id
+# class method that selects all cats from db and returns cat instances
+# instance method that saves a cat to the db if it is not already saved
 
 class Cat
     attr_accessor :id, :name, :age
 
-    # @@all = []
-
-    def initialize(name, age)
+    def initialize(id = nil, name, age)
+        @id = id
         @name = name
         @age = age
-        # @@all << self
+    end
+
+    def self.all
+        sql = <<-SQL
+        SELECT * FROM cats;
+        SQL
+        self.create_from_db(DB[:conn].execute(sql))
+    end
+
+    def self.create_from_db(rows)
+        rows.map do |row|
+            Cat.new(*row)
+        end
     end
 
     def self.create_table
@@ -28,19 +41,27 @@ class Cat
         SELECT * FROM cats
         WHERE id = ?
         SQL
-        DB[:conn].execute(sql, id)
-        binding.pry
+        cat_array = DB[:conn].execute(sql, id).flatten
+        self.new(*cat_array)
+    end
+
+    def self.find_by(attr_name, attr_value)
+        # more abstract version of find_by_id
     end
 
     def save
-        # refactor this method to only save a cat if it doesn't already exist
-        sql = <<-SQL
-        INSERT INTO cats (name, age)
-        VALUES (?, ?)
-        SQL
-        DB[:conn].execute(sql, self.name, self.age)
-        @id = DB[:conn].execute('SELECT last_insert_rowid() FROM cats')[0][0]
-        self
+        if !id
+            sql = <<-SQL
+            INSERT INTO cats (name, age)
+            VALUES (?, ?)
+            SQL
+            DB[:conn].execute(sql, self.name, self.age)
+            @id = DB[:conn].execute('SELECT last_insert_rowid() FROM cats')[0][0]
+            self
+        else
+            puts 'already saved'
+            self
+        end
     end
 
 end
